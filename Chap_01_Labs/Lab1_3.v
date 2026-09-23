51a6d79ea75de37c96c8a4917e0b2f73d5a10f32
muhammad-hammad15 <muhammad.hammad@impare.cloud>
2026-09-23 07:02:53 +0500

    Update Lab1_3.v

--- Chap_01_Labs/Lab1_3.v
+++ Chap_01_Labs/Lab1_3.v
@@ -1,7 +1,7 @@
 module mux2x1 ( input  A,
-	        input  B, 
-		input  SEL, 
-		output Y);
+	        	input  B, 
+				input  SEL, 
+				output Y);
 
 // Continuous assignment 
 assign Y = SEL? B : A;
